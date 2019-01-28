8ceebc94f7fd7fe8c2eca8cd61bbecf599c6fd68
Prakash <Prakash@vikingairtech.com>
2019-01-28 14:04:01 +0800

    Initial commit

---
+++ app/src/main/java/com/icondev76/myapplication/DataModel.java
@@ -0,0 +1,14 @@
+package com.icondev76.myapplication;
+
+public class DataModel {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
